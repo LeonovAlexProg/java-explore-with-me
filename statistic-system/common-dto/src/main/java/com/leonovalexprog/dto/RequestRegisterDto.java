13d713ca062424520cdfd4aae566e03f9c87088e
Alex <leonovalexprog@gmail.com>
2023-07-11 21:02:40 +0300

    upd: исправить десериализацию дат в теле запроса

--- statistic-system/common-dto/src/main/java/com/leonovalexprog/dto/RequestRegisterDto.java
+++ statistic-system/common-dto/src/main/java/com/leonovalexprog/dto/RequestRegisterDto.java
@@ -1,15 +1,21 @@
 package com.leonovalexprog.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RequestRegisterDto {
     private String app;
     private String uri;
     private String ip;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
