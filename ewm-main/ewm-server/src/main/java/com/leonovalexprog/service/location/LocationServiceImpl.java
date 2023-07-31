0bd553171c2b8738074edae385ef2a9b05f52774
Alex <leonovalexprog@gmail.com>
2023-07-31 22:48:43 +0300

    upd: исправить codestyle + постман тесты

--- ewm-main/ewm-server/src/main/java/com/leonovalexprog/service/location/LocationServiceImpl.java
+++ ewm-main/ewm-server/src/main/java/com/leonovalexprog/service/location/LocationServiceImpl.java
@@ -161,8 +161,7 @@ public class LocationServiceImpl implements LocationService {
 
         if ((lat1 == lat2) && (lon1 == lon2)) {
             return 0.0;
-        }
-        else {
+        } else {
             double theta = lon1 - lon2;
             double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
             dist = Math.acos(dist);
