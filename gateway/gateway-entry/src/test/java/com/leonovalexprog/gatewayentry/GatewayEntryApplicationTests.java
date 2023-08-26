844cee0b7b848338039384df120fb2c76158dd28
Alex <leonovalexprog@gmail.com>
2023-08-26 05:14:09 +0300

    upd: add gateway module

---
+++ gateway/gateway-entry/src/test/java/com/leonovalexprog/gatewayentry/GatewayEntryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonovalexprog.gatewayentry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayEntryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
